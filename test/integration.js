8843c460dd514b3990eb49c80a8463f7c5271f29
KCM <knightedcodemonkey@gmail.com>
2026-01-01 20:05:34 -0600

    test: smoke tests for --mode full, none.

--- test/integration.js
+++ test/integration.js
@@ -141,6 +141,58 @@ describe('duel', () => {
     assert.ok(logged(spy, 1).includes('--detect-dual-package-hazard expects'))
   })
 
+  it('builds with --mode none', async t => {
+    t.after(async () => {
+      await rmDist(plainDist)
+    })
+
+    await duel(['-p', 'test/__fixtures__/plain/tsconfig.json', '--mode', 'none'])
+
+    assert.ok(existsSync(resolve(plainDist, 'index.js')))
+    assert.ok(existsSync(resolve(plainDist, 'cjs/index.cjs')))
+
+    const cjs = await readFile(join(plainDist, 'cjs/index.cjs'), 'utf8')
+    assert.ok(!cjs.includes('import type'))
+    assert.ok(!cjs.includes('assert {'))
+    assert.match(cjs, /require\(['"]\.\/enforce\.cjs['"]\)/)
+
+    const { status: statusEsm } = spawnSync('node', [join(plainDist, 'index.js')], {
+      stdio: 'inherit',
+    })
+    const { status: statusCjs } = spawnSync('node', [join(plainDist, 'cjs/index.cjs')], {
+      stdio: 'inherit',
+    })
+
+    assert.equal(statusEsm, 0)
+    assert.equal(statusCjs, 0)
+  })
+
+  it('builds with --mode full', async t => {
+    t.after(async () => {
+      await rmDist(plainDist)
+    })
+
+    await duel(['-p', 'test/__fixtures__/plain/tsconfig.json', '--mode', 'full'])
+
+    assert.ok(existsSync(resolve(plainDist, 'index.js')))
+    assert.ok(existsSync(resolve(plainDist, 'cjs/index.cjs')))
+
+    const cjs = await readFile(join(plainDist, 'cjs/index.cjs'), 'utf8')
+    assert.ok(!cjs.includes('import type'))
+    assert.ok(!cjs.includes('assert {'))
+    assert.match(cjs, /require\(['"]\.\/enforce\.cjs['"]\)/)
+
+    const { status: statusEsm } = spawnSync('node', [join(plainDist, 'index.js')], {
+      stdio: 'inherit',
+    })
+    const { status: statusCjs } = spawnSync('node', [join(plainDist, 'cjs/index.cjs')], {
+      stdio: 'inherit',
+    })
+
+    assert.equal(statusEsm, 0)
+    assert.equal(statusCjs, 0)
+  })
+
   it('creates a dual CJS build while transforming module globals', async t => {
     const spy = t.mock.method(global.console, 'log')
 
